c9ac32c6e349940403a8e8d5ab849f6a7ebc2854
38tter <odradek38@gmail.com>
2025-06-02 01:29:53 +0900

    Initial Commit

---
+++ beer_analyzer/beer_analyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  beer_analyzerApp.swift
+//  beer_analyzer
+//
+//  Created by 宮田聖也 on 2025/06/02.
+//
+
+import SwiftUI
+
+@main
+struct beer_analyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ beer_analyzerTests/beer_analyzerTests.swift
@@ -0,0 +1,16 @@
+//
+//  beer_analyzerTests.swift
+//  beer_analyzerTests
+//
+//  Created by 宮田聖也 on 2025/06/02.
+//
+
+import Testing
+
+struct beer_analyzerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
